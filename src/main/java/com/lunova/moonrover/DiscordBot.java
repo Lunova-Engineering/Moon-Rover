77837555daf8eaf01de0a0cc3d2ae107ce3fb062
metorrite <drakeforness@gmail.com>
2023-09-19 22:45:08 -0700

    Initializing Commit
    Initial code setup

---
+++ src/main/java/com/lunova/moonrover/DiscordBot.java
@@ -0,0 +1,8 @@
+package com.lunova.moonrover;
+
+public class DiscordBot {
+
+    public static void main(String[] args) {
+        System.out.println("This is the start of our discord bot!");
+    }
+}
