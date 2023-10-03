a601b59de60ee4cfdc8a7c3f20722b376f85c20c
metorrite <drakeforness@gmail.com>
2023-10-02 20:29:41 -0700

    Pushing!
    Testing webhooks!

--- src/main/java/com/lunova/moonrover/logger/Logger.java
+++ src/main/java/com/lunova/moonrover/logger/Logger.java
@@ -1,12 +1,11 @@
 package com.lunova.moonrover.logger;
 
+
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.slf4j.LoggerFactory;
 
 public class Logger {
 
-
-
     public static void logMessage(MessageReceivedEvent event) {
-
     }
 }
