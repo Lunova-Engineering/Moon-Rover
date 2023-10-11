b863a23aebdee91ff6ef0336afacbb93b67d5e34
metorrite <drakeforness@gmail.com>
2023-10-11 04:07:58 -0700

    Refactor of BotCommand

--- src/main/java/com/lunova/moonbot/commands/BotCommand.java
+++ src/main/java/com/lunova/moonbot/commands/BotCommand.java
@@ -9,24 +9,24 @@ import java.util.List;
 
 public abstract class BotCommand {
 
-    private final String identifier;
+    private final String name;
 
-    public BotCommand(String identifier) {
-        this.identifier = identifier;
+    public BotCommand(String name) {
+        this.name = name;
     }
-    public String getIdentifier() {
-        return identifier;
+    public String getName() {
+        return name;
     }
 
     public static void deregister(RestAction<List<Command>> commandList, BotCommand botCommand) {
         commandList.queue(commands -> {
             commands.stream()
-                    .filter(command -> command.getName().equalsIgnoreCase(botCommand.getIdentifier()))
+                    .filter(command -> command.getName().equalsIgnoreCase(botCommand.getName()))
                     .findAny()
                     .ifPresentOrElse(
                             commandToDelete -> commandToDelete.delete().queue(),
                             () -> {
-                                throw new IllegalArgumentException("Command not found for identifier: " + botCommand.getIdentifier());
+                                throw new IllegalArgumentException("Command not found for identifier: " + botCommand.getName());
                             }
                     );
         }, throwable -> {
